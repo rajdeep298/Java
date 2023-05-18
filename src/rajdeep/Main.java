7efada715794bfafddcb6e315b493273109e1e5b
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-05-19 01:23:27 +0530

    Some mild changes

--- src/rajdeep/Main.java
+++ src/rajdeep/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+        System.out.println(input.nextLine());
     }
 }
---
+++ src/rajdeep/Primitives.java
@@ -0,0 +1,21 @@
+package rajdeep;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno=64;
+
+        //String is not primitive
+        String name ="Rajdeep";
+
+        char letter ='R';
+
+        float num=45.56f;
+
+
+        double big_num=1466.65;
+
+        long large_integer=2365496596L;
+
+        boolean check = true;
+    }
+}
