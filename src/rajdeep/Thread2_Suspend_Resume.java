d2ae17968e431da3ce9f52ec46d92b68728ae966
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-12-30 12:25:47 +0530

    some mild changes

---
+++ src/rajdeep/Thread2_Suspend_Resume.java
@@ -0,0 +1,18 @@
+package rajdeep;
+
+public class Thread2_Suspend_Resume extends Thread{
+    public void run(){
+        System.out.println("Thread is running.");
+    }
+
+    public static void main(String[] args) {
+        Thread2_Suspend_Resume t1=new Thread2_Suspend_Resume();
+        t1.start();
+
+        t1.suspend();
+        System.out.println("Thread is suspended.");
+
+        t1.resume();
+        System.out.println("Thread is resumed.");
+    }
+}
