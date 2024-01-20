08eb922f7b278964cfd41bd7abf466e89f8dffcd
rajdeep298 <rajdeepkundu2983@gmail.com>
2024-01-20 20:19:17 +0530

    some mild changes

--- src/rajdeep/CC12_Assignment/Assignment17_VowelException.java
+++ src/rajdeep/CC12_Assignment/Assignment17_VowelException.java
@@ -1,27 +1,34 @@
 package rajdeep.CC12_Assignment;
 
-public class Assignment17_VowelException extends Exception {
-    Assignment17_VowelException(String s) {
-        super(s);
+class vowelException extends Exception {
+    String str;
+    vowelException(String str) {
+        this.str = str;
     }
+    void checkVowel() throws vowelException {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    count++;
+                }
+            }
+        }
+        if (count == 0) {
+            throw new vowelException(str);
+        }
+    }
+}
+public class Assignment17_VowelException {
 
     public static void main(String[] args) {
         String str = "Brght, crsp wntr wnds whstl crclng drftng flcks.";
-        int count = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                count++;
-            }
-        }
-
         try {
-            if (count == 0) {
-                throw new Assignment17_VowelException("No vowels found");
-            }
-        } catch (Assignment17_VowelException e) {
-            System.out.println("Exception caught: " + e.getMessage());
+            vowelException ve = new vowelException(str);
+            ve.checkVowel();
+        } catch (vowelException e) {
+            System.out.println("Exception caught: " + e);
         }
     }
 }
